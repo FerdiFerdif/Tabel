741107fa22d2bb0b25bd307e9ce586dff6ac30f3
FerdiFerdif <42802261+FerdiFerdif@users.noreply.github.com>
2018-12-03 10:35:40 +0100

    Opdracht 1.1

---
+++ JavaScript/HF1/op1java.js
@@ -0,0 +1,18 @@
+
+var a = 10;
+if(a > 9) {
+    console.log("bro.... you sure about that")
+}
+
+for (var i = 0; i <11; i++) {
+    console.log("i heeft nu de waarde: " + i);
+    
+}
+
+var mijnauto =  {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "Blauw"
+}
+console.log("De kleur van de auto is: " + mijnauto.kleur);
