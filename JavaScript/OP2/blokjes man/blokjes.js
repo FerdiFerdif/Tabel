69604947d9c17a663ceab9d76200e6429a6c926b
FerdiFerdif <42802261+FerdiFerdif@users.noreply.github.com>
2018-12-04 11:24:58 +0100

    Opdracht 2.2 BLOKJES FERDI

---
+++ JavaScript/OP2/blokjes man/blokjes.js
@@ -0,0 +1,27 @@
+let Blokje = document.getElementById("b1");
+Blokje.style.background = "blue";
+
+Blokje.style.position = "absolute";
+Blokje.style.top = "0px";
+
+let Blokje2 = document.getElementById("b2");
+Blokje2.style.background = "green";
+
+Blokje2.style.position = "absolute";
+Blokje2.style.top = "20px";
+Blokje2.style.left = "100px"
+
+let Blokje3 = document.getElementById("b3");
+Blokje3.style.background = "yellow";
+
+Blokje3.style.position = "absolute";
+Blokje3.style.top = "40px";
+Blokje3.style.left = "200px"
+
+
+let Blokje4 = document.getElementById("b4");
+Blokje4.style.background = "red";
+
+Blokje4.style.position = "absolute";
+Blokje4.style.top = "0px";
+Blokje4.style.left = "300px"
